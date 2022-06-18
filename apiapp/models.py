2df572f5f2375c044be2c51b1c0d4126f56216ce
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-06-18 23:01:47 +0300

    create models

--- apiapp/models.py
+++ apiapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class personalDetails(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    Age = models.IntegerField()
+    gender = models.CharField(max_length=30)
+    occupation = models.CharField(max_length=50)
+    campus = models.CharField(max_length=50)
+    bio = models.TextField()
+    
+    
+    def __str__(self):
+        return self.first_name
